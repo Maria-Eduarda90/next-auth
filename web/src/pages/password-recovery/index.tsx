3191d8401561317457c4d93007a77b76838879d9
Mary Skywalker <snowdennaruto90@gmail.com>
2022-10-23 23:09:41 -0400

    completed the password recovery screen

--- web/src/pages/password-recovery/index.tsx
+++ web/src/pages/password-recovery/index.tsx
@@ -1,7 +1,31 @@
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
 import styles from './styles.module.scss';
 
 export default function PasswordRecovery(){
     return(
-        <h1>Hello</h1>
+        <div className={styles.container}>
+            <aside className={styles.aside} />
+
+            <main>
+                <form>
+                    <div>
+                        <h1>Esqueceu a senha?</h1>
+                        <h4>Digite seu e-mail e você receberá um link para alterar sua senha.</h4>
+                    </div>
+
+                    <section>
+                        <div>
+                            <label htmlFor="">Endereço de email</label>
+                            <Input type="text" placeholder="johndoe@example.com" />
+                        </div>
+                    </section>
+
+                    <Button type="submit">
+                        Recuperar senha
+                    </Button>
+                </form>
+            </main>
+        </div>
     );
 }
