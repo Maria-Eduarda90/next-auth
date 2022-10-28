b545c6341262a854354b6af2fb0b2c1aeeae2b70
Mary Skywalker <snowdennaruto90@gmail.com>
2022-10-28 15:17:07 -0400

    feat: creation file and user login finished

--- backend/app/Controllers/Http/AuthController.ts
+++ backend/app/Controllers/Http/AuthController.ts
@@ -1,35 +1,45 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import User from 'App/Models/User'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import User from 'App/Models/User';
 
 export default class AuthController {
-  public async login({ request, auth }: HttpContextContract) {
-    const email = request.input('email')
-    const password = request.input('password')
+  public async login({ request, auth, response }: HttpContextContract) {
+    try {
+      const email = request.input('email')
+      const password = request.input('password')
 
-    const token = await auth.use('api').attempt(email, password)
+      const token = await auth.use('api').attempt(email, password)
 
-    return token.toJSON()
+      return token.toJSON()
+    } catch {
+      return response.badRequest('Invalid credentials');
+    }
   }
 
-  public async register({ request }: HttpContextContract) {
+  public async register({ request, response }: HttpContextContract) {
     try {
-        const { 
-            firstname, 
-            secondname,
-            email, 
-            password 
-        } = request.only(['firstname', 'secondname', 'email', 'password']);
+      const { firstname, secondname, email, password } = request.only([
+        'firstname',
+        'secondname',
+        'email',
+        'password',
+      ])
 
-        const createUser = await User.create({
-            firstname,
-            secondname,
-            email,
-            password
-        })
+      const createUser = await User.create({
+        firstname,
+        secondname,
+        email,
+        password,
+      })
 
-        return createUser;
-    } catch (err){
-        console.log(err);
+      return createUser
+    } catch {
+      return response.status(201).json({ message: 'invalid' })
     }
+  }
+
+  public async index() {
+    const user = await User.all()
+
+    return user
   }
 }
