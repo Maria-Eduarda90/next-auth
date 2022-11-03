79ceeb9318a6238f61e0dbe1d7c48f2a56db2ff8
Mary Skywalker <snowdennaruto90@gmail.com>
2022-11-02 21:38:55 -0400

    structure of the function that takes what the user types in the input

--- web/src/components/Login/index.tsx
+++ web/src/components/Login/index.tsx
@@ -3,28 +3,46 @@ import { Aside } from '../Aside';
 import { Button } from '../Button';
 import { Input } from '../Input';
 import styles from './styles.module.scss';
+import { useForm } from 'react-hook-form';
 
 export function Login(){
+    const { register, handleSubmit } = useForm();
+
+    function handleSignIn(data){
+        console.log(data);
+    }
+
     return(
         <div className={styles.container}>
             <Aside/>
 
             <main>
-                <form>
-                    <div>
-                        <h1>Seja bem vindo!</h1>
-                        <h4>Faça login em sua conta e comece a usar</h4>
-                    </div>
-
+                <div className={styles.title}>
+                    <h1>Seja bem vindo!</h1>
+                    <h4>Faça login em sua conta e comece a usar</h4>
+                </div>
+                <form onSubmit={handleSubmit(handleSignIn)}>
                     <section>
                         <div>
                             <label htmlFor="">Endereço de email</label>
-                            <Input type="text" placeholder="johndoe@example.com" />
+                            <input
+                             className={styles.input}
+                             {...register('email')}
+                             name="email"
+                             type="email" 
+                             placeholder="johndoe@example.com" 
+                            />
                         </div>
 
                         <div>
                             <label htmlFor="">Sua senha</label>
-                            <Input type="password" placeholder="**************" />
+                            <input
+                                className={styles.input}
+                                {...register('password')}
+                                name="password"
+                                type="password"
+                                placeholder="**************" 
+                            />
                         </div>
                     </section>
 
@@ -37,7 +55,7 @@ export function Login(){
                     <Link href="/password-recovery">
                         <a>Esqueceu sua senha?</a>
                     </Link>
-                    <Link href="/sign-in">
+                    <Link href="/sign-up">
                         Não possui uma conta? Crie uma agora!
                     </Link>
                 </div>
