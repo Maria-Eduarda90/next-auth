c52606d2f2c954e4824c5d07775a4097d7bc9fbe
Mary Skywalker <snowdennaruto90@gmail.com>
2022-11-03 23:32:55 -0400

    private routes, get information SSR

--- web/src/pages/blogs/index.tsx
+++ web/src/pages/blogs/index.tsx
@@ -1,5 +1,32 @@
+import { GetServerSideProps } from 'next';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import { parseCookies } from 'nookies';
+
 export default function Blogs(){
+    const { user } = useContext(AuthContext);
     return(
-        <h1>Blogs</h1>
+        <>
+            <h1>{user?.firstname}</h1>
+            <h1>{user?.secondname}</h1>
+            <h1>{user?.email}</h1>
+        </>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const { ['nextauth.token']: token } = parseCookies(ctx);
+
+    if(!token){
+        return{
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+
+    return {
+        props: {}
+    }
 }
