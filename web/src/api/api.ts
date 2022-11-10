216e5218705f1296e8afb8ac01671d0253f2492e
Mary Skywalker <snowdennaruto90@gmail.com>
2022-11-09 21:33:01 -0400

    removing some lines of code

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -1,22 +1,3 @@
-import axios from "axios";
-import { parseCookies } from 'nookies'; 
+import { getApiClient } from './apiClient'
 
-const { 'nextauth.token': token } = parseCookies();
-
-const api = axios.create({
-  baseURL: "http://127.0.0.1:3333/api/",
-});
-
-if(token){
-  // api.defaults.headers["Authorization"] = `Bearer ${token}`;
-  api.interceptors.request.use(config => {
-    config.headers = {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    };
-
-    return config;
-  });
-}
-
-export default api;
+export const api = getApiClient();
