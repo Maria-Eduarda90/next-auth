1335cdd32fcc256e227c3d909a45b3a564b215cc
Mary Skywalker <snowdennaruto90@gmail.com>
2022-11-09 21:34:24 -0400

    configuring the avatar file

--- web/src/components/Avatar/index.tsx
+++ web/src/components/Avatar/index.tsx
@@ -1,10 +1,15 @@
 import styles from './styles.module.scss';
+import Image from 'next/image'
 
 export function Avatar(){
     return(
         <div className={styles.containerAvatar}>
             <main className={styles.main}>
-                <img src="https://github.com/Maria-Eduarda90.png" alt="" className={styles.avatar} />
+                <img
+                 src={"https://github.com/Maria-Eduarda90.png"}
+                 alt={"avatar"}
+                 className={styles.avatar} 
+                />
             </main>
         </div>
     );
