5de34b95825b44112c0d15241d9935f802e43247
Mary Skywalker <snowdennaruto90@gmail.com>
2022-11-09 21:38:48 -0400

    configuration proposed by nextjs itself

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,13 +1,27 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { AuhtProvider } from '../context/AuthContext';
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+
+  if (typeof window === 'undefined') {
+    return <></>;
+  } else {
   return (
     <AuhtProvider>
       <Component {...pageProps} />
     </AuhtProvider>
   );
+  }
 }
 
 export default MyApp
