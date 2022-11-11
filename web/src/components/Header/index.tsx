81b4004e9f1ee153feeaa2d665809a755e831544
Mary Skywalker <snowdennaruto90@gmail.com>
2022-11-10 21:06:32 -0400

    modified

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,18 +1,25 @@
 import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
+import { useAvatar } from '../../hooks/useAvatar';
 import { Avatar } from '../Avatar';
 import { DropdownMenu } from '../DropdownMenu';
+import { Profile } from '../Profile';
 import styles from './styles.module.scss';
 
 export function Header(){
     const { user } = useContext(AuthContext);
+    const { avatar } = useAvatar();
+
     return(
         <header className={styles.header}>
             <div className={styles.wrapper}>
                 <h1>Blog</h1>
 
                 <div className={styles.content}>
-                    <Avatar />
+                    {/* {
+                        avatar ? <Avatar /> : <Profile />
+                    } */}
+                    <Profile />
                     <p>{user?.firstname} {user?.secondname}</p>
                     <DropdownMenu />
                 </div>
