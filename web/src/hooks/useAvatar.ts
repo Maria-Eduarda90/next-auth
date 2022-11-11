b789ef8ea5cdfce01972a56261f3876f5ba51f56
Mary Skywalker <snowdennaruto90@gmail.com>
2022-11-10 21:12:07 -0400

    added hooks

---
+++ web/src/hooks/useAvatar.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect, useContext } from "react";
+import { api } from "../api/api";
+import { AuthContext } from "../context/AuthContext";
+
+export function useAvatar(){
+    const [avatar, setAvatar] = useState("");
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        api.get(`image/${user?.id}`).then((response) => {
+            setAvatar(response.data);
+        })
+    })
+
+    return {
+        avatar,
+    }
+}
