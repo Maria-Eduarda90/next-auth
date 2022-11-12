7a864d182d31925383f8f030ebb4067fe6f434d9
Mary Skywalker <snowdennaruto90@gmail.com>
2022-11-11 23:11:35 -0400

    blog creation form created

---
+++ web/src/components/FormModalBlog/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+export function FormModalBlog(){
+    return(
+        <div className={styles.modal}>
+            <form>
+                
+            </form>
+        </div>
+    );
+}
