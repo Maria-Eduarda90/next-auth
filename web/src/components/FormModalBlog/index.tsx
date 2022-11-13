45334a2a7386b9a18e8bf31a43da32a39b84ef85
Mary Skywalker <snowdennaruto90@gmail.com>
2022-11-12 22:29:46 -0400

    blog creation form, completed

--- web/src/components/FormModalBlog/index.tsx
+++ web/src/components/FormModalBlog/index.tsx
@@ -1,11 +1,30 @@
 import styles from './styles.module.scss';
 
-export function FormModalBlog(){
+type FormModalBlogType = {
+    onClick: () => void;
+}
+
+export function FormModalBlog(props: FormModalBlogType){
     return(
         <div className={styles.modal}>
-            <form>
-                
+            <h1>Crie um blog de sua preferência :)</h1>
+            <form >
+                <fieldset>
+                    <legend>Url do Blog</legend>
+                    <input type="url" name="" id="" />
+                </fieldset>
+                <fieldset>
+                    <legend>Titulo completo do Blog</legend>
+                    <input type="text" name="" id="" />
+                </fieldset>
+                <fieldset>
+                    <legend>Descrição completa do Blog</legend>
+                    <textarea />
+                </fieldset>
+
+                <button>Criar blog</button>
             </form>
+            <button {...props}>Cancelar</button>
         </div>
     );
 }
