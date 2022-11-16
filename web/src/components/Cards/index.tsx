54b308523567a6c2cc852b8aea0c02f2b24449e3
Mary Skywalker <snowdennaruto90@gmail.com>
2022-11-15 23:10:29 -0400

    feat: passing properties to component cards

--- web/src/components/Cards/index.tsx
+++ web/src/components/Cards/index.tsx
@@ -1,40 +1,31 @@
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { api } from '../../api/api';
-import { BlogType } from '../../data/@types/Blog';
+
 import { TextUtils } from '../../utils/TextUtils';
 import styles from './styles.module.scss';
 
-export function Cards(){
-    const [blogs, setBlogs] = useState<BlogType[]>([]);
+export type CardsType = {
+    url: string;
+    title: string;
+    description: string;
+    href: string;
+}
+
+export function Cards({ url, title, description, href }: CardsType){
     const maximumTextSize = 200;
-
-    useEffect(() => {
-        api.get("/blog").then(response => {
-            setBlogs(response.data);
-        })
-    }, [blogs])
-
     return(
-        <div className={styles.cardContainer}>
-            {blogs.map(blog => {
-                return(
-                    <div className={styles.card} key={blog.id}>
-                        <img src={blog.url} alt="" />
+        <div className={styles.card}>
+            <img src={url} alt="" />
 
-                        <div>
-                            <h1>{blog.title}</h1>
-                            <p>
-                                {TextUtils.limitText(`${blog.description}`, maximumTextSize)}
-                            </p>
+            <div>
+                <h1>{title}</h1>
+                <p>
+                    {TextUtils.limitText(`${description}`, maximumTextSize)}
+                </p>
 
-                            <Link href="#">
-                                Ver mais+
-                            </Link>
-                        </div>
-                    </div> 
-                );
-            })}   
-        </div>
+                <Link href={href}>
+                    Ver mais+
+                </Link>
+            </div>
+        </div>     
     );
 }
